7216a2562685c5178f4a23d5451273faf3bd2ed3
phuong <phuongb.go1.com.au>
2016-03-15 14:17:57 +0700

    Refactor: h5peditor-text.js

--- js/h5peditor-text.js
+++ js/h5peditor-text.js
@@ -1,108 +1,111 @@
 var H5PEditor = H5PEditor || {};
-var ns = H5PEditor;
 
-/**
- * Create a text field for the form.
- *
- * @param {mixed} parent
- * @param {Object} field
- * @param {mixed} params
- * @param {function} setValue
- * @returns {ns.Text}
- */
-ns.Text = function (parent, field, params, setValue) {
-  this.field = field;
-  this.value = params;
-  this.setValue = setValue;
-  this.changeCallbacks = [];
-};
+H5PEditor.Text = (function ($, H5P) {
 
-/**
- * Append field to wrapper.
- *
- * @param {type} $wrapper
- * @returns {undefined}
- */
-ns.Text.prototype.appendTo = function ($wrapper) {
-  var that = this;
+  /**
+   * Create a text field for the form.
+   *
+   * @param {mixed} parent
+   * @param {Object} field
+   * @param {mixed} params
+   * @param {function} setValue
+   * @returns {ns.Text}
+   */
+  function WidgetText(parent, field, params, setValue) {
+    this.field = field;
+    this.value = params;
+    this.setValue = setValue;
+    this.changeCallbacks = [];
+  };
 
-  this.$item = ns.$(this.createHtml()).appendTo($wrapper);
-  this.$input = this.$item.children('label').children('input');
-  this.$errors = this.$item.children('.h5p-errors');
+  /**
+   * Append field to wrapper.
+   *
+   * @param {type} $wrapper
+   * @returns {undefined}
+   */
+  WidgetText.prototype.appendTo = function ($wrapper) {
+    var that = this;
 
-  this.$input.change(function () {
-    // Validate
-    var value = that.validate();
+    this.$item = H5PEditor.$(this.createHtml()).appendTo($wrapper);
+    this.$input = this.$item.children('label').children('input');
+    this.$errors = this.$item.children('.h5p-errors');
 
-    if (value !== false) {
-      // Set param
-      if (H5P.trim(value) === '') {
-        // Avoid storing empty strings. (will be valid if field is optional)
-        delete that.value;
-        that.setValue(that.field);
+    this.$input.change(function () {
+      // Validate
+      var value = that.validate();
+
+      if (value !== false) {
+        // Set param
+        if (H5P.trim(value) === '') {
+          // Avoid storing empty strings. (will be valid if field is optional)
+          delete that.value;
+          that.setValue(that.field);
+        }
+        else {
+          that.value = value;
+          that.setValue(that.field, H5PEditor.htmlspecialchars(value));
+        }
+
+        for (var i = 0; i < that.changeCallbacks.length; i++) {
+          that.changeCallbacks[i](value);
+        }
       }
-      else {
-        that.value = value;
-        that.setValue(that.field, ns.htmlspecialchars(value));
-      }
+    });
+  };
 
-      for (var i = 0; i < that.changeCallbacks.length; i++) {
-        that.changeCallbacks[i](value);
-      }
+  /**
+   * Run callback when value changes.
+   *
+   * @param {function} callback
+   * @returns {Number|@pro;length@this.changeCallbacks}
+   */
+  WidgetText.prototype.change = function (callback) {
+    this.changeCallbacks.push(callback);
+    callback();
+
+    return this.changeCallbacks.length - 1;
+  };
+
+  /**
+   * Create HTML for the text field.
+   */
+  WidgetText.prototype.createHtml = function (callback) {
+    var input = H5PEditor.createText(this.value, this.field.maxLength, this.field.placeholder);
+    var label = H5PEditor.createLabel(this.field, input);
+
+    return H5PEditor.createItem(this.field.type, label, this.field.description);
+  };
+
+  /**
+   * Validate the current text field.
+   */
+  WidgetText.prototype.validate = function () {
+    var that = this;
+
+    var value = H5P.trim(this.$input.val());
+
+    if ((that.field.optional === undefined || !that.field.optional) && !value.length) {
+      this.$errors.append(H5PEditor.createError(H5PEditor.t('core', 'requiredProperty', {':property': 'text field'})));
     }
-  });
-};
+    else if (value.length > this.field.maxLength) {
+      this.$errors.append(H5PEditor.createError(H5PEditor.t('core', 'tooLong', {':max': this.field.maxLength})));
+    }
+    else if (this.field.regexp !== undefined && value.length && !value.match(new RegExp(this.field.regexp.pattern, this.field.regexp.modifiers))) {
+      this.$errors.append(H5PEditor.createError(H5PEditor.t('core', 'invalidFormat')));
+    }
 
-/**
- * Run callback when value changes.
- *
- * @param {function} callback
- * @returns {Number|@pro;length@this.changeCallbacks}
- */
-ns.Text.prototype.change = function (callback) {
-  this.changeCallbacks.push(callback);
-  callback();
+    return H5PEditor.checkErrors(this.$errors, this.$input, value);
+  };
 
-  return this.changeCallbacks.length - 1;
-};
+  /**
+   * Remove this item.
+   */
+  WidgetText.prototype.remove = function () {
+    this.$item.remove();
+  };
 
-/**
- * Create HTML for the text field.
- */
-ns.Text.prototype.createHtml = function () {
-  var input = ns.createText(this.value, this.field.maxLength, this.field.placeholder);
-  var label = ns.createLabel(this.field, input);
-
-  return ns.createItem(this.field.type, label, this.field.description);
-};
-
-/**
- * Validate the current text field.
- */
-ns.Text.prototype.validate = function () {
-  var that = this;
-
-  var value = H5P.trim(this.$input.val());
-
-  if ((that.field.optional === undefined || !that.field.optional) && !value.length) {
-    this.$errors.append(ns.createError(ns.t('core', 'requiredProperty', {':property': 'text field'})));
-  }
-  else if (value.length > this.field.maxLength) {
-    this.$errors.append(ns.createError(ns.t('core', 'tooLong', {':max': this.field.maxLength})));
-  }
-  else if (this.field.regexp !== undefined && value.length && !value.match(new RegExp(this.field.regexp.pattern, this.field.regexp.modifiers))) {
-    this.$errors.append(ns.createError(ns.t('core', 'invalidFormat')));
-  }
-
-  return ns.checkErrors(this.$errors, this.$input, value);
-};
-
-/**
- * Remove this item.
- */
-ns.Text.prototype.remove = function () {
-  this.$item.remove();
-};
+}(jQuery, H5P));
 
 // Tell the editor what widget we are.
-ns.widgets.text = ns.Text;
+H5PEditor.widgets.text = H5PEditor.Text;
