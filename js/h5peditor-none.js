c81b187f9f6bdec22695079d68afb42ed5f9281a
phuong <phuongb.go1.com.au>
2016-03-15 11:54:42 +0700

    GO1P-3017: H5P editor API proof of concept

--- js/h5peditor-none.js
+++ js/h5peditor-none.js
@@ -1,54 +1,59 @@
 var H5PEditor = H5PEditor || {};
-var ns = H5PEditor;
 
 /**
- * Create a field without html
- * 
- * @param {mixed} parent
- * @param {object} field
- * @param {mixed} params
- * @param {function} setValue
+ * Construct a none from library semantics.
  */
-ns.None = function (parent, field, params, setValue) {
-  this.parent = parent;
-  this.field = field;
-  this.params = params;
-  this.setValue = setValue;
-};
+H5PEditor.None = (function ($) {
+  
+  /**
+   * Create a field without html
+   * 
+   * @param {mixed} parent
+   * @param {object} field
+   * @param {mixed} params
+   * @param {function} setValue
+   */
+  function WidgetNone(parent, field, params, setValue) {
+    this.parent = parent;
+    this.field = field;
+    this.params = params;
+    this.setValue = setValue;
+  };
 
-/**
- * Implementation of appendTo
- * 
- * None doesn't append anything
- * 
- * @param {jQuery} $wrapper
- */
-ns.None.prototype.appendTo = function ($wrapper) {};
+  /**
+   * Implementation of appendTo
+   * 
+   * None doesn't append anything
+   * 
+   * @param {jQuery} $wrapper
+   */
+  WidgetNone.prototype.appendTo = function ($wrapper) {
+  };
 
-/**
- * Implementation of validate
- * 
- * None allways validates
- */
-ns.None.prototype.validate = function () {
-  return true;
-};
+  /**
+   * Implementation of validate
+   * 
+   * None allways validates
+   */
+  WidgetNone.prototype.validate = function () {
+    return true;
+  };
 
-/**
- * Collect functions to execute once the tree is complete.
- * 
- * @param {function} ready
- */
-ns.None.prototype.ready = function (ready) {
-  this.parent.ready(ready);
-};
+  /**
+   * Collect functions to execute once the tree is complete.
+   * 
+   * @param {function} ready
+   */
+  WidgetNone.prototype.ready = function (ready) {
+    this.parent.ready(ready);
+  };
 
-/**
- * Remove this item.
- */
-ns.None.prototype.remove = function () {
-  ns.removeChildren(this.children);
-};
+  /**
+   * Remove this item.
+   */
+  WidgetNone.prototype.remove = function () {
+    removeChildren(this.children);
+  };
 
-// Tell the editor what widget we are.
-ns.widgets.none = ns.None;
+  ns.widgets.none = WidgetNone;
+});
