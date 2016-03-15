a94aed1650f74a5cf27d5a7a124964044360f6a8
phuong <phuongb.go1.com.au>
2016-03-15 16:42:12 +0700

    refactor: h5peditor-editor.js

--- js/h5peditor-editor.js
+++ js/h5peditor-editor.js
@@ -4,201 +4,208 @@
 var H5PEditor = (H5PEditor || {});
 var ns = H5PEditor;
 
-/**
- * Construct the editor.
- *
- * @class H5PEditor.Editor
- * @param {string} library
- * @param {Object} defaultParams
- */
-ns.Editor = function (library, defaultParams, replace) {
-  var self = this;
 
-  // Create iframe and replace the given element with it
-  var iframe = ns.$('<iframe/>', {
-    css: {
-      display: 'block',
-      width: '100%',
-      height: '3em',
-      border: 'none',
-      zIndex: 101,
-      top: 0,
-      left: 0
-    },
-    'class': 'h5p-editor-iframe',
-    frameBorder: '0'
-  }).replaceAll(replace).load(function () {
-    var $ = this.contentWindow.H5P.jQuery;
-    var LibrarySelector = this.contentWindow.H5PEditor.LibrarySelector;
-    this.contentWindow.H5P.$body = $(this.contentDocument.body);
-    var $container = $('body > .h5p-editor');
+H5PEditor.Editor = (function ($) {
+  /**
+   * Construct the editor.
+   *
+   * @class H5PEditor.Editor
+   * @param {string} library
+   * @param {Object} defaultParams
+   */
+  WidgetEditor = function (library, defaultParams, replace) {
+    var self = this;
 
-    // Load libraries list
-    $.ajax({
-      dataType: 'json',
-      url: ns.getAjaxUrl('libraries')
-    }).fail(function () {
-      $container.html('Error, unable to load libraries.');
-    }).done(function (data) {
-      self.selector = new LibrarySelector(data, library, defaultParams);
-      self.selector.appendTo($container.html(''));
-      if (library) {
-        self.selector.$selector.change();
-      }
-    });
+    // Create iframe and replace the given element with it
+    var iframe = $('<iframe/>', {
+      css: {
+        display: 'block',
+        width: '100%',
+        height: '3em',
+        border: 'none',
+        zIndex: 101,
+        top: 0,
+        left: 0
+      },
+      'class': 'h5p-editor-iframe',
+      frameBorder: '0'
+    }).replaceAll(replace).load(function () {
+      var $ = this.contentWindow.H5P.jQuery;
+      var LibrarySelector = this.contentWindow.H5PEditor.LibrarySelector;
+      this.contentWindow.H5P.$body = $(this.contentDocument.body);
+      var $container = $('body > .h5p-editor');
 
-    // Start resizing the iframe
-    if (iframe.contentWindow.MutationObserver !== undefined) {
-      // If supported look for changes to DOM elements. This saves resources.
-      var running;
-      var limitedResize = function (mutations) {
-        if (!running) {
-          running = setTimeout(function () {
-            resize();
-            running = null;
-          }, 40); // 25 fps cap
+      // Load libraries list
+      $.ajax({
+        dataType: 'json',
+        url: ns.getAjaxUrl('libraries')
+      }).fail(function () {
+        $container.html('Error, unable to load libraries.');
+      }).done(function (data) {
+        self.selector = new LibrarySelector(data, library, defaultParams);
+        self.selector.appendTo($container.html(''));
+        if (library) {
+          self.selector.$selector.change();
         }
-      };
-
-      new iframe.contentWindow.MutationObserver(limitedResize).observe(iframe.contentWindow.document.body, {
-        childList: true,
-        attributes: true,
-        characterData: true,
-        subtree: true,
-        attributeOldValue: false,
-        characterDataOldValue: false
       });
-      H5P.$window.resize(limitedResize);
-    }
-    else {
-      // Use an interval for resizing the iframe
-      (function resizeInterval() {
-        resize();
-        setTimeout(resizeInterval, 40); // No more than 25 times per second
-      })();
-    }
-  }).get(0);
-  iframe.contentDocument.open();
-  iframe.contentDocument.write(
-    '<!doctype html><html>' +
-    '<head>' +
-      ns.wrap('<link rel="stylesheet" href="', ns.assets.css, '">') +
-      ns.wrap('<script src="', ns.assets.js, '"></script>') +
-    '</head><body>' +
-      '<div class="h5p-editor">' + ns.t('core', 'loading', {':type': 'libraries'}) + '</div>' +
-    '</body></html>');
-  iframe.contentDocument.close();
-  iframe.contentDocument.documentElement.style.overflow = 'hidden';
+
+      // Start resizing the iframe
+      if (iframe.contentWindow.MutationObserver !== undefined) {
+        // If supported look for changes to DOM elements. This saves resources.
+        var running;
+        var limitedResize = function (mutations) {
+          if (!running) {
+            running = setTimeout(function () {
+              resize();
+              running = null;
+            }, 40); // 25 fps cap
+          }
+        };
+
+        new iframe.contentWindow.MutationObserver(limitedResize).observe(iframe.contentWindow.document.body, {
+          childList: true,
+          attributes: true,
+          characterData: true,
+          subtree: true,
+          attributeOldValue: false,
+          characterDataOldValue: false
+        });
+        H5P.$window.resize(limitedResize);
+      }
+      else {
+        // Use an interval for resizing the iframe
+        (function resizeInterval() {
+          resize();
+          setTimeout(resizeInterval, 40); // No more than 25 times per second
+        })();
+      }
+    }).get(0);
+    iframe.contentDocument.open();
+    iframe.contentDocument.write(
+      '<!doctype html><html>' +
+      '<head>' +
+        ns.wrap('<link rel="stylesheet" href="', ns.assets.css, '">') +
+        ns.wrap('<script src="', ns.assets.js, '"></script>') +
+      '</head><body>' +
+        '<div class="h5p-editor">' + ns.t('core', 'loading', {':type': 'libraries'}) + '</div>' +
+      '</body></html>');
+    iframe.contentDocument.close();
+    iframe.contentDocument.documentElement.style.overflow = 'hidden';
+
+    /**
+     * Checks if iframe needs resizing, and then resize it.
+     *
+     * @private
+     */
+    var resize = function () {
+      if (iframe.clientHeight === iframe.contentDocument.body.scrollHeight &&
+          iframe.contentDocument.body.scrollHeight === iframe.contentWindow.document.body.clientHeight) {
+        return; // Do not resize unless page and scrolling differs
+      }
+
+      // Retain parent size to avoid jumping/scrolling
+      var parentHeight = iframe.parentElement.style.height;
+      iframe.parentElement.style.height = iframe.parentElement.clientHeight + 'px';
+
+      // Reset iframe height, in case content has shrinked.
+      iframe.style.height = iframe.contentWindow.document.body.clientHeight + 'px';
+
+      // Resize iframe so all content is visible. Use scrollHeight to make sure we get everything
+      iframe.style.height = iframe.contentDocument.body.scrollHeight + 'px';
+
+      // Free parent
+      iframe.parentElement.style.height = parentHeight;
+    };
+  };
 
   /**
-   * Checks if iframe needs resizing, and then resize it.
+   * Find out which library is used/selected.
    *
-   * @private
+   * @alias H5PEditor.Editor#getLibrary
+   * @returns {string} Library name
    */
-  var resize = function () {
-    if (iframe.clientHeight === iframe.contentDocument.body.scrollHeight &&
-        iframe.contentDocument.body.scrollHeight === iframe.contentWindow.document.body.clientHeight) {
-      return; // Do not resize unless page and scrolling differs
+  WidgetEditor.prototype.getLibrary = function () {
+    if (this.selector !== undefined) {
+      return this.selector.$selector.val();
     }
-
-    // Retain parent size to avoid jumping/scrolling
-    var parentHeight = iframe.parentElement.style.height;
-    iframe.parentElement.style.height = iframe.parentElement.clientHeight + 'px';
-
-    // Reset iframe height, in case content has shrinked.
-    iframe.style.height = iframe.contentWindow.document.body.clientHeight + 'px';
-
-    // Resize iframe so all content is visible. Use scrollHeight to make sure we get everything
-    iframe.style.height = iframe.contentDocument.body.scrollHeight + 'px';
-
-    // Free parent
-    iframe.parentElement.style.height = parentHeight;
   };
-};
 
-/**
- * Find out which library is used/selected.
- *
- * @alias H5PEditor.Editor#getLibrary
- * @returns {string} Library name
- */
-ns.Editor.prototype.getLibrary = function () {
-  if (this.selector !== undefined) {
-    return this.selector.$selector.val();
-  }
-};
-
-/**
- * Get parameters needed to start library.
- *
- * @alias H5PEditor.Editor#getParams
- * @returns {Object} Library parameters
- */
-ns.Editor.prototype.getParams = function () {
-  if (this.selector !== undefined) {
-    return this.selector.getParams();
-  }
-};
-
-/**
- * Editor translations index by library name or "core".
- *
- * @member {Object} H5PEditor.language
- */
-ns.language = {};
-
-/**
- * Translate text strings.
- *
- * @method H5PEditor.t
- * @param {string} library The library name(machineName), or "core".
- * @param {string} key Translation string identifier.
- * @param {Object} vars Placeholders and values to replace in the text.
- * @returns {string} Translated string, or a text if string translation is missing.
- */
-ns.t = function (library, key, vars) {
-  if (ns.language[library] === undefined) {
-    return 'Missing translations for library ' + library;
-  }
-
-  var translation;
-  if (library === 'core') {
-    if (ns.language[library][key] === undefined) {
-      return 'Missing translation for ' + key;
+  /**
+   * Get parameters needed to start library.
+   *
+   * @alias H5PEditor.Editor#getParams
+   * @returns {Object} Library parameters
+   */
+  WidgetEditor.prototype.getParams = function () {
+    if (this.selector !== undefined) {
+      return this.selector.getParams();
     }
-    translation = ns.language[library][key];
-  }
-  else {
-    if (ns.language[library].libraryStrings === undefined || ns.language[library].libraryStrings[key] === undefined) {
-      return ns.t('core', 'missingTranslation', {':key': key});
+  };
+
+  /**
+   * Editor translations index by library name or "core".
+   *
+   * @member {Object} H5PEditor.language
+   */
+  H5PEditor.language = {};
+
+  /**
+   * Translate text strings.
+   *
+   * @method H5PEditor.t
+   * @param {string} library The library name(machineName), or "core".
+   * @param {string} key Translation string identifier.
+   * @param {Object} vars Placeholders and values to replace in the text.
+   * @returns {string} Translated string, or a text if string translation is missing.
+   */
+  H5PEditor.t = function (library, key, vars) {
+    if (H5PEditor.language[library] === undefined) {
+      return 'Missing translations for library ' + library;
     }
-    translation = ns.language[library].libraryStrings[key];
-  }
 
-  // Replace placeholder with variables.
-  for (var placeholder in vars) {
-    if (!vars[placeholder]) {
-      continue;
+    var translation;
+    if (library === 'core') {
+      if (H5PEditor.language[library][key] === undefined) {
+        return 'Missing translation for ' + key;
+      }
+      translation = H5PEditor.language[library][key];
     }
-    translation = translation.replace(placeholder, vars[placeholder]);
-  }
+    else {
+      if (H5PEditor.language[library].libraryStrings === undefined || H5PEditor.language[library].libraryStrings[key] === undefined) {
+        return H5PEditor.t('core', 'missingTranslation', {':key': key});
+      }
+      translation = H5PEditor.language[library].libraryStrings[key];
+    }
 
-  return translation;
-};
+    // Replace placeholder with variables.
+    for (var placeholder in vars) {
+      if (!vars[placeholder]) {
+        continue;
+      }
+      translation = translation.replace(placeholder, vars[placeholder]);
+    }
 
-/**
- * Wraps multiple content between a prefix and a suffix.
- *
- * @method H5PEditor.wrap
- * @param {string} prefix Inserted before the content.
- * @param {Array} content List of content to be wrapped.
- * @param {string} suffix Inserted after the content.
- * @returns {string} All content put together with prefix and suffix.
- */
-ns.wrap = function (prefix, content, suffix) {
-  var result = '';
-  for (var i = 0; i < content.length; i++) {
-    result += prefix + content[i] + suffix;
-  }
-  return result;
-};
+    return translation;
+  };
+
+  /**
+   * Wraps multiple content between a prefix and a suffix.
+   *
+   * @method H5PEditor.wrap
+   * @param {string} prefix Inserted before the content.
+   * @param {Array} content List of content to be wrapped.
+   * @param {string} suffix Inserted after the content.
+   * @returns {string} All content put together with prefix and suffix.
+   */
+  H5PEditor.wrap = function (prefix, content, suffix) {
+    var result = '';
+    for (var i = 0; i < content.length; i++) {
+      result += prefix + content[i] + suffix;
+    }
+    return result;
+  };
+
+}(jQuery));
+
+
+
